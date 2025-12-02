a7585ebfac625e113d162d6dd1c51c1d587084d3
ri. <32203242+arirureroo@users.noreply.github.com>
2025-12-02 17:30:23 +0700

    fix: update footer links and enhance table styling for tariff descriptions

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -14,6 +14,7 @@ export default ((opts?: Options) => {
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
+          Made by <a href="https://github.com/arirureroo">ri.</a> |{" "}
           {i18n(cfg.locale).components.footer.createdWith}{" "}
           <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
         </p>
